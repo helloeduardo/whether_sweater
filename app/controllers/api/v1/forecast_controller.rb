a8fae532bb38b5a52a7079209ead52c392cfb0b4
Eduardo Parra <helloeduardo@icloud.com>
2021-01-18 12:14:55 -0700

    Add facade to controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    forecast = ''
+    forecast = ForecastFacade.forecast(params[:location])
     render json: ForecastSerializer.new(forecast)
   end
 end
