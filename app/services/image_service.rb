3ca60637a3b35f2eb9dd71f3da516aa99fc1dc2a
Eduardo Parra <helloeduardo@icloud.com>
2021-01-18 21:59:10 -0700

    Add image service

---
+++ app/services/image_service.rb
@@ -0,0 +1,15 @@
+class ImageService
+  def self.image(location)
+    response = conn.get('/search/photos') do |req|
+      req.params["query"] = location
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: 'https://api.unsplash.com') do |f|
+      f.params["client_id"] = ENV["IMAGE_API_KEY"]
+    end
+  end
+end
