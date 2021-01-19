ac3813d556e5c1f22c742de392b4844e65938abc
Eduardo Parra <helloeduardo@icloud.com>
2021-01-18 22:28:16 -0700

    Pass correct image data

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -1,6 +1,6 @@
 class ImageFacade
   def self.image(location)
-    image_details = ImageService.image(location)
+    image_details = ImageService.image(location)[:results].first
     Image.new(image_details, location)
   end
 end
