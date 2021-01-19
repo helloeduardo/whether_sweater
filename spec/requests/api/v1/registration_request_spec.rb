5d7848f5133961ccb4c71656d0d5245d4991578a
Eduardo Parra <helloeduardo@icloud.com>
2021-01-18 23:02:49 -0700

    Add registration spec

---
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe 'User Registration Endpoint', type: :request do
+  it 'can register a user', :vcr do
+    body = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+
+    headers = {
+      'Content_Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    post '/api/v1/users', headers: headers, body: JSON.generate(body)
+
+    expect(response).to be_successful
+
+    user = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(user).to have_key(:id)
+    expect(user[:id]).to be_an Integer
+
+    expect(user).to have_key(:type)
+    expect(user[:type]).to eq('users')
+
+    expect(user).to have_key(:attributes)
+    expect(user[:attributes]).to be_a(Hash)
+
+    attributes = user[:attributes]
+
+    expect(attributes).to have_key(:email)
+    expect(attributes[:email]).to be_a(String)
+
+    expect(attributes).to have_key(:api_key)
+    expect(attributes[:api_key]).to be_a(String)
+  end
+end
