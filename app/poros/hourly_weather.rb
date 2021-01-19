c757078d9fe54bbb9d42158c11d3a786acf1642a
Eduardo Parra <helloeduardo@icloud.com>
2021-01-18 17:17:58 -0700

    Add weather poros

---
+++ app/poros/hourly_weather.rb
@@ -0,0 +1,20 @@
+class HourlyWeather
+  attr_reader :time, :temperature, :wind_speed, :wind_direction, :conditions, :icon
+
+  def initialize(data)
+    @time = Time.at(data[:dt]).strftime('%H:%M:%S')
+    @temperature = data[:temp]
+    @wind_speed = "#{data[:wind_speed]} mph"
+    @wind_direction = "from #{degree_to_direction(data[:wind_deg])}"
+    @conditions = data[:weather][0][:description]
+    @icon = data[:weather][0][:icon]
+  end
+
+  def degree_to_direction(degree)
+    directions = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+
+    value = (degree/22.5) + 0.5
+
+    directions[value.to_i % 16]
+  end
+end
