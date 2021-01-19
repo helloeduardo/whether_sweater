15427046bb4976ab8db736eea4f522c23b350009
Eduardo Parra <helloeduardo@icloud.com>
2021-01-19 15:46:05 -0700

    Add user sad path tests

--- spec/requests/api/v1/registration_request_spec.rb
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -34,4 +34,70 @@ RSpec.describe 'User Registration Endpoint', type: :request do
     expect(attributes).to have_key(:api_key)
     expect(attributes[:api_key]).to be_a(String)
   end
+
+  it 'can return an error when duplicate users exist' do
+    body = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+
+    headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    post '/api/v1/users', headers: headers, params: body.to_json
+    post '/api/v1/users', headers: headers, params: body.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    user = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user[:email]).to eq(["has already been taken"])
+  end
+
+  it 'can return an error with mismatched password' do
+    body = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "messed up password"
+    }
+
+    headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    post '/api/v1/users', headers: headers, params: body.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    user = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user[:password_confirmation]).to eq(["doesn't match Password"])
+  end
+
+  it 'can return an error with missing field' do
+    body = {
+      "password": "password",
+      "password_confirmation": "password"
+    }
+
+    headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    post '/api/v1/users', headers: headers, params: body.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    user = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user[:email]).to eq(["can't be blank"])
+  end
 end
