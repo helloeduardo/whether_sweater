adcc855d395afc99df72f584d590f74e4fd6ca02
Eduardo Parra <helloeduardo@icloud.com>
2021-01-19 15:36:35 -0700

    Add have_secure password and token validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
+    it { should have_secure_password }
+    it { should have_secure_token :api_key }
   end
 end
