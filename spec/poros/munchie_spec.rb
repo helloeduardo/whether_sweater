e0dd1961acfcc9addb20193568adafbeec9ec0ff
Eduardo Parra <helloeduardo@icloud.com>
2021-01-19 10:41:49 -0700

    Add poro spec for Munchie

---
+++ spec/poros/munchie_spec.rb
@@ -0,0 +1,34 @@
+RSpec.describe Munchie do
+  it "exists and has attributes", :vcr do
+    response = File.read('spec/fixtures/poros/food_sample_response.json')
+
+    location = 'denver,co'
+    food = JSON.parse(response, symbolize_names: true)
+    arrival_time = (Time.now + 18000).to_i
+
+    data = {
+      destination_city: location,
+      arrival_time: arrival_time,
+      weather: ForecastFacade.forecast(location),
+      food: food[:businesses].first
+    }
+
+    munchie = Munchie.new(data)
+
+    expect(munchie).to be_a Munchie
+    expect(munchie.destination_city).to eq(location)
+    expect(munchie.travel_time).to be_a String
+
+    expect(munchie.forecast).to be_a Hash
+    expect(munchie.forecast).to have_key :summary
+    expect(munchie.forecast[:summary]).to be_a String
+    expect(munchie.forecast).to have_key :temperature
+    expect(munchie.forecast[:temperature]).to be_a Numeric
+
+    expect(munchie.restaurant).to be_a Hash
+    expect(munchie.restaurant).to have_key :name
+    expect(munchie.restaurant[:name]).to be_a String
+    expect(munchie.restaurant).to have_key :address
+    expect(munchie.restaurant[:address]).to be_a String
+  end
+end
